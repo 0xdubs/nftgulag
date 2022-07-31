02d332fb6e4b894ce58e6deebe350cda880e1e33
danielw1534 <39487081+danielw1534@users.noreply.github.com>
2022-07-31 13:36:15 -0500

    Update AppConfig.ts
    Removed unnecessary apostrophe.

--- utils/AppConfig.ts
+++ utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'NFT Gulag',
   title: 'NFT Gulag',
-  description: 'NFT Gulag - where NFT`s come to compete',
+  description: 'NFT Gulag - where NFTs come to compete',
   locale: 'en',
 };
